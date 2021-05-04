1375c98bd26f43021d81277da69c0a4055f251e7
aronbj20 <aronbj20@ru.is>
2021-05-04 11:26:44 +0000

    postgres

--- ship_o_cereal/settings.py
+++ ship_o_cereal/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ship_o_cereal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ship-o-cereal-db',
+        'USER': 'ship-o-cereal-db-user',
+        'PASSWORD': 'admin123',
+        'HOST': '34.105.153.20',
+        'PORT': '5432',
     }
 }
 
