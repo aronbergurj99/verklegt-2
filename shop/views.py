2ad68854bad4eeae14a000780ec2f23c2a0fd88a
Aron Bergur Jóhannsson <aronbj20>
2021-05-04 17:22:48 +0000

    small products

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Product, Rating, ProductImage
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    return render(request, 'shop/index.html', {
+        "products": Product.objects.all().order_by('name'),
+        "ratings": Rating.objects.all(),
+        "images": ProductImage.objects.all().order_by('name')
+    })
