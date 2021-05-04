f0a1cc6d91fe7960af62089aa5afa413a98739f7
Aron Bergur Jóhannsson <aronbj20>
2021-05-04 13:44:47 +0000

    fixing rating model

--- ship_o_cereal/settings.py
+++ ship_o_cereal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -138,6 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # This is were images will be uploaded
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = [
-    BASE_DIR / 'media',
-]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Type(models.Model):
     type = models.CharField(max_length=50)
 
+    def __str__(self):
+        return '{}'.format(self.type)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=80)
@@ -13,17 +16,25 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     stock = models.PositiveIntegerField()
 
+    def __str__(self):
+        return '{}'.format(self.name)
+
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='products', blank=True, null=True)
 
+    def __str__(self):
+        return '{}'.format(self.name)
+
 
 class Rating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     rating = models.DecimalField(decimal_places=2, max_digits=4)
     number_of_ratings = models.PositiveIntegerField()
 
-
+    def __str__(self):
+        return '{}'.format(self.product.name)
 
 
