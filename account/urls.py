1b1bafc80fbd2857eb46f9953bac44b952e3501f
Davíð Örn Harðarson <davidh20@ru.is>
2021-05-07 11:37:27 +0000

    Register and Login running

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('profile', views.profile, name="profile"),
+    path('register', views.register, name="register"),
+    path('login', LoginView.as_view(template_name='account/login.html'), name='login')
 ]
