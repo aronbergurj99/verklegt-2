c43db8bb39a738c5860ebabf371f9ee359909e40
aronbj20 <aronbj20@ru.is>
2021-05-10 11:59:26 +0000

    live search refactor

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,7 @@
 
 var products;
-var images;
-var searchResults = document.getElementById("search-result");
-var searchInput = document.getElementById("search");
+let searchResults = document.getElementById("search-result");
+let searchInput = document.getElementById("search");
 let filteredArr = []
 //function to retrieve all products when searching
 
@@ -11,9 +10,7 @@ function getProducts() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(this.response)
-      products = data['products']
-      images = data['images']
+      products = JSON.parse(this.response).data
       search()
     }
   };
@@ -23,7 +20,7 @@ function getProducts() {
 
 
 
-function createSearchResult(product, image) {
+function createSearchResult(product) {
   var href = document.createElement("a");
   href.href = product.id
 
@@ -37,7 +34,7 @@ function createSearchResult(product, image) {
   productPrice.innerHTML = "Price: " + product.price;
 
   var productImage = document.createElement("img");
-  productImage.setAttribute("src", "/media/" + image.image.toString())
+  productImage.setAttribute("src", product.image)
   productImage.className = "x-small-images"
   container.appendChild(name);
   container.appendChild(productPrice);
@@ -49,17 +46,10 @@ function createSearchResult(product, image) {
 
 function search() {
   searchResults.innerHTML = ""
-
   filteredArr = products.filter(info => info['name'].toLowerCase().includes(searchInput.value.toLowerCase()));
-  console.log(products);
-  console.log(images)
-  for (var i = 0; i < filteredArr.length; i++) {
-    for (var j=0; j < images.length; j++) {
-      if (images[j].product == filteredArr[i].id) {
-        createSearchResult(filteredArr[i], images[j]);
-      }
-    }
 
+  for (let i = 0; i < filteredArr.length; i++) {
+    createSearchResult(filteredArr[i]);
   }
 }
 
