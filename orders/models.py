687e8bdd1614af8d3a0355f4ef43674c4629b5b4
Davíð Örn Harðarson <davidh20@ru.is>
2021-05-14 10:04:18 +0000

    Added price calculations to orders

--- orders/models.py
+++ orders/models.py
@@ -7,7 +7,7 @@ class Orders(models.Model):
     status = models.CharField(max_length=80)
     date = models.DateTimeField(auto_now_add=True)
     paid = models.BooleanField(blank=True)
-    total_price = models.DecimalField(decimal_places=2, max_digits=5)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10)
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
     country = models.CharField(max_length=80)
